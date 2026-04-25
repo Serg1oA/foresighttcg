d557ea7a367bfe26e401731d40d81d4c063de1e2
Sérgio Almeida <almeida.sergiomc@gmail.com>
2026-04-25 21:06:22 +0000

    Make cookies function now async

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,11 +7,20 @@ export async function GET(request: Request) {
   const code = searchParams.get('code')
 
   if (code) {
-    const cookieStore = cookies()
+    const cookieStore = await cookies()
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      { cookies: { getAll: () => cookieStore.getAll(), setAll: (cookiesToSet) => { cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options)) } } }
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+      {
+        cookies: {
+          getAll: () => cookieStore.getAll(),
+          setAll: (cookiesToSet) => {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          }
+        }
+      }
     )
     await supabase.auth.exchangeCodeForSession(code)
   }
