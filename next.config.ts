b3dcd3d91209d114441028f1d590b8ed46f4bf9e
Sérgio Almeida <almeida.sergiomc@gmail.com>
2026-04-26 21:17:27 +0000

    Add supabase to tsconfig exclude list

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ['**/supabase/**'],
+    }
+    return config
+  },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['./supabase/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
