df10ad01ef804e70c1ef7c13362807a44e99f597
Sérgio Almeida <almeida.sergiomc@gmail.com>
2026-04-26 21:23:41 +0000

    Remove exp. block since tsconfig handles it

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': ['./supabase/**/*'],
-    },
-  },
 };
 
 export default nextConfig;
